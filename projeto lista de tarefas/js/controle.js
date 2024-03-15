fac57c309381cdd72c72522d2fc978179765530d
gustavito <gustavorb34@hotmail.com>
2024-03-15 12:13:20 -0300

    o fim é para os fracos

---
+++ projeto lista de tarefas/js/controle.js
@@ -0,0 +1,25 @@
+let input = document.getElementById("inputTarefa");
+let btnAdd = document.getElementById("btn-add");
+let main = document.getElementById("areaLista")
+
+function addTarefa() {
+    // pegar o valor digitado no input
+    let valorInput = input.value;
+
+    // se o input não for vazio, nulo e indefinido
+    if ((valorInput !== "") && (valorInput !== null) && (valorInput !== undefined)) {
+        let novoItem = `<div class="item">
+        <div class="item-icone">
+            <span class="mdi mdi-circle-outline"></span>
+        </div>
+        <div class="item-nome">
+            ${valorInput}
+        </div>
+        <div class="item-botao">
+            <button class="delete"><span class="mdi mdi-delete"></span>Deletar</button>
+        </div>`
+
+        main.innerHTML += novoItem;
+
+    }
+}
